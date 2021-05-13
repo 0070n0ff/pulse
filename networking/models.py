00ed8ec753ae6bb67defad668efa750f32ab2ab8
root <0070n0ff@gmail.com>
2021-05-13 14:58:19 +0500

    renew 20210513

--- networking/models.py
+++ networking/models.py
@@ -48,8 +48,8 @@ class RegGate(MyModel):
 class Network(MyModel):
     LOCAL = []
     for x in range(1, 31):
-        # f=str(x)
         LOCAL.append((x, '/' + str(x)))
+
     cidr = models.PositiveIntegerField("CIDR", choices=LOCAL, default=24)
     rg = models.ManyToManyField(RegGate, blank=True)
     type = models.ForeignKey(DevType, on_delete=models.CASCADE, blank=True)
@@ -66,26 +66,19 @@ class Network(MyModel):
         verbose_name_plural = "Network"
 
 
-@receiver(post_save, sender=Network)
-def create_ip_addr(sender, instance:Network, created, **kwargs):
-     if created:
-
-        subnet = ipaddress.ip_network(instance.toStr())
-
-        for ipn in subnet.hosts():
-            ip1=IPAddr(name=ipn,dns_name="blank",owner=instance.owner, net=instance)
-            ip1.save()
 
 
 # ip-address
 class IPAddr(MyModel):
-    net = models.ForeignKey(Network, on_delete=models.PROTECT,blank=True)
     dns_name = models.CharField("DNS name", max_length=15, default="blank")
-
+    netw = models.ForeignKey(Network, on_delete=models.PROTECT, blank=True)
     def __str__(self):
-        return "%s (%s): %s/%i" % (self.name, self.dns_name, self.net.name, self.net.cidr)
+        return "%s (%s)" % (self.name, self.dns_name)
 
-
+    class Meta:
+        verbose_name = "IPAddr"
+        verbose_name_plural = "IPAddr"
+#PC
 class PC(MyModel):
     SN = models.CharField("S/N", max_length=32, blank=True)
     type = models.ForeignKey(DevType, on_delete=models.PROTECT, blank=True)
@@ -101,7 +94,7 @@ class SW(MyModel):
 class NetPCInt(MyModel):
     net_obj = models.ForeignKey(PC, on_delete=models.CASCADE)
     mac = models.CharField("MAC:", max_length=25, blank=True)
-    ip = models.ForeignKey(IPAddr, on_delete=models.PROTECT,blank=True)
+    ip = models.ForeignKey(IPAddr, on_delete=models.CASCADE,blank=True)
     #ip = models.ManyToManyField(IPAddr,blank=True, related_name="ipaddr")
 
     def __str__(self):
@@ -111,7 +104,17 @@ class NetPCInt(MyModel):
 class NetSWInt(MyModel):
     net_obj = models.ForeignKey(SW, on_delete=models.CASCADE)
     mac = models.CharField("MAC:", max_length=25, blank=True)
-    ip = models.ForeignKey(IPAddr, on_delete=models.PROTECT,blank=True)
+    ip = models.ForeignKey(IPAddr, on_delete=models.CASCADE,blank=True)
 
     def __str__(self):
         return "%s (%s) : %s" % (self.net_obj.name, self.name, self.ip.name)
+
+@receiver(post_save, sender=Network)
+def create_ip_addr(sender, instance:Network, created, **kwargs):
+     if created:
+
+        subnet = ipaddress.ip_network(instance.tostr())
+
+        for ipn in subnet.hosts():
+            ip1=IPAddr(name=ipn, dns_name="blank", owner=instance.owner, netw=instance)
+            ip1.save()
